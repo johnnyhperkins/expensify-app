fbf87227c4afd6562611980b0526de6dc7b0bcef
Johnny Perkins <Johnny@Johnnys-MacBook-Air.local>
2018-04-03 18:33:10 -0400

    Updated expense list item and added numeral library to format expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export const ExpenseListItem = ({description, amount, createdAt, id}) => (
     <div>
         <h3><Link to={"/edit/" + id}>Description: {description}</Link></h3>
-        <p>Amount: {amount} - Created At: {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM Do YYYY')}
+        </p>
     </div>
 );
 
